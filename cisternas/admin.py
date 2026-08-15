5de040297134fb14b3b613dbc84c5d5ec8cefb74
pedraodsl <grandoleandro482@gmail.com>
2026-08-15 16:50:09 -0300

    Configura models no Django Admin

--- cisternas/admin.py
+++ cisternas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    Usuario,
+    Cisterna,
+    Monitoramento,
+    Relatorio,
+    Alerta,
+    Abastecimento,
+)
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Cisterna)
+admin.site.register(Monitoramento)
+admin.site.register(Relatorio)
+admin.site.register(Alerta)
+admin.site.register(Abastecimento)
