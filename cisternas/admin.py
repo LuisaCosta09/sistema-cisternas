b35ae057d157455c31d453201be60af1c2e1d933
Ravany Costa <ravanycostadacruz@gmail.com>
2026-08-17 22:47:56 -0300

    Organiza Django Admin

--- cisternas/admin.py
+++ cisternas/admin.py
@@ -1,16 +1,47 @@
 from django.contrib import admin
-from .models import (
-    Usuario,
-    Cisterna,
-    Monitoramento,
-    Relatorio,
-    Alerta,
-    Abastecimento,
-)
+from .models import Usuario, Cisterna, Monitoramento, Relatorio, Alerta, Abastecimento
 
-admin.site.register(Usuario)
-admin.site.register(Cisterna)
-admin.site.register(Monitoramento)
-admin.site.register(Relatorio)
-admin.site.register(Alerta)
-admin.site.register(Abastecimento)
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("id", "user")
+    search_fields = ("user__username", "user__email")
+
+
+@admin.register(Cisterna)
+class CisternaAdmin(admin.ModelAdmin):
+    list_display = ("id", "localizacao", "capacidade", "status", "usuario")
+    list_filter = ("status",)
+    search_fields = ("localizacao", "descricao")
+
+
+@admin.register(Monitoramento)
+class MonitoramentoAdmin(admin.ModelAdmin):
+    list_display = ("id", "cisterna", "dataHora", "nivelAgua", "consumo", "situacao")
+    list_filter = ("situacao", "dataHora")
+
+
+@admin.register(Relatorio)
+class RelatorioAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "usuario",
+        "cisterna",
+        "periodoInicio",
+        "periodoFim",
+        "formato",
+        "dataGeracao",
+    )
+    list_filter = ("formato", "dataGeracao")
+
+
+@admin.register(Alerta)
+class AlertaAdmin(admin.ModelAdmin):
+    list_display = ("id", "cisterna", "tipo", "status", "dataHora")
+    list_filter = ("tipo", "status")
+
+
+@admin.register(Abastecimento)
+class AbastecimentoAdmin(admin.ModelAdmin):
+    list_display = ("id", "cisterna", "dataHora", "quantidadeAgua", "tipo", "status")
+    list_filter = ("tipo", "status")
